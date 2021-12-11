de69f2d6d21e7028a03ce4ae29a124a8285f9ca9
rakhaegg <rakhae.g@gmail.com>
2021-12-11 20:03:46 +0700

    master

---
+++ src/validator/users/index.js
@@ -0,0 +1,20 @@
+/*
+    * Fungsi validateUserPayload merupakan fungsi yang dibuat untuk memvalidasi data payload (dari parameternya) 
+    * berdasarkan UserPayloadSchema yang sudah kita buat di schema.js. Bila objek payload tidak sesuai dengan skema 
+    * yang sudah kita tetapkan, maka fungsi tersebut akan membangkitkan InvariantError.
+
+*/
+const InvariantError = require('../../exception/InvariantError');
+const { UserPayloadSchema } = require('./schema');
+ 
+const UsersValidator = {
+  validateUserPayload: (payload) => {
+    const validationResult = UserPayloadSchema.validate(payload);
+ 
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = UsersValidator
